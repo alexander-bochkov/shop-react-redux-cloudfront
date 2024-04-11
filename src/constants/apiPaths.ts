7ad52e25b9aa1a011475eaa9a44b53cf69187d34
Aleksandr Bochkov <aleksandr_bochkov@epam.com>
2024-04-11 22:33:45 +0400

    Update import path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://ihvunnac5m.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://7xxluksp0l.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://ivwjs6gs69.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
