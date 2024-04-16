42717b2cba4e782dc0dea65454102d02f01488c9
Aleksandr Bochkov <aleksandr_bochkov@epam.com>
2024-04-16 10:07:50 +0400

    Complete task 7

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -28,12 +28,18 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
 
     console.log("uploadFile to", url);
 
+    const authorization_token =
+      localStorage.getItem("authorization_token") || "";
+
     // Get the presigned URL
     const response = await axios({
       method: "GET",
       url,
       params: {
         name: encodeURIComponent(file.name),
+      },
+      headers: {
+        Authorization: `Basic ${btoa(authorization_token)}`,
       },
     });
     console.log("File to upload: ", file.name);
